f217cfeb922e3fefd369d759ca5c5348a7bbe349
martproz <martinprozapas@gmail.com>
2022-04-03 16:44:26 -0300

    03 04 22

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,8 @@
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
-import '../UI/Card.css';
 
 const ExpenseItem = (props) => {
   return (
@@ -11,7 +12,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button>Change title</button>
     </Card>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import '../src/index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "../src/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 
